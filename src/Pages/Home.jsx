8b365e716e90494f5166158d5944782985c8f41f
ARVINDSHUKLA0 <shuklaarvind2521@gmail.com>
2025-03-01 09:49:47 +0530

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -20,6 +20,12 @@ import Footer from '../Component/Footer'
 const Home = () => {
 
   const [imegshover, setImgesHover] = useState(AwardsOne)
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleToggle = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
 
   const Imgesfunc = (image) => {
     setImgesHover(image)
@@ -29,29 +35,30 @@ const Home = () => {
     { creativeHediing: 'Design Blueprinting', creativeArtical: 'I translate strategic plans into actionable visual frameworks. This involves creating wireframes, style guides, and mockups to map out design elements and user flows, ensuring a clear and efficient design process.' },
     { creativeHediing: 'Creative Development', creativeArtical: 'I transform concepts into compelling visuals. I explore diverse styles, experiment with innovative techniques, and refine ideas to deliver unique, impactful designs that capture attention and resonate with your audience.' },
     { creativeHediing: 'Project Handover:', creativeArtical: 'I deliver comprehensive, organized files with clear documentation, ensuring seamless implementation. I provide style guides, asset packages, and any necessary training, empowering you to effectively utilize the design deliverables.', },
-    
+
 
   ];
 
-   
+
 
   const MainArr = [
     {
-      name: "Brand Identity:", year: "2024", SerivesHedding: "digital infinity awards", titel: 'best animation', image: AwardsOne,
+      name: "Brand Identity:", brandDescription: "What I do : I craft the visual foundation of your brand. This includes designing a unique logo that captures your essence, developing comprehensive brand guidelines (color palettes, typography, imagery style), and establishing a cohesive visual identity that resonates with your target audience.", brandInnercontent: "Why it matters : A strong brand identity builds trust, creates recognition, and sets you apart from competitors. It provides consistency across all communication channels, ensuring a unified and professional brand image.", year: "2024", SerivesHedding: "digital infinity awards", titel: 'best animation', image: AwardsOne,
     },
     {
-      name: "Web Design", year: "2023", SerivesHedding: "best website of the year", titel: 'interactive design', image: AwardTwo,
+      name: "Web Design", brandDescription: "What I do: I design user-friendly and visually appealing websites that are tailored to your business goals. This involves creating intuitive website layouts, designing engaging user interfaces (UI), and ensuring responsive design for optimal viewing on all devices.", brandInnercontent: "Why it matters: Your website is your online storefront. A well-designed website enhances user experience, drives conversions, and establishes your online credibility.", year: "2023", SerivesHedding: "best website of the year", titel: 'interactive design', image: AwardTwo,
     },
     {
-      name: "Print Design",  year: "2022", SerivesHedding: "festival of digital culture", titel: 'photo contest', image: AwardThree,
+      name: "Print Design", brandDescription: "What I do: I create tangible marketing materials that leave a lasting impression. This includes designing brochures, flyers, business cards, stationery, and packaging that effectively communicate your message and reflect your brand's personality", brandInnercontent: "Why it matters: Print materials provide a physical connection with your audience. High-quality print design reinforces your brand and creates a professional and memorable experience.", year: "2022", SerivesHedding: "festival of digital culture", titel: 'photo contest', image: AwardThree,
     },
     {
-      name: "Digital Graphics", year: "2021", SerivesHedding: "crown digital agency site", titel: 'css design awards', image: Awardfour,
+      name: "Digital Graphics", brandDescription: "What I do: I design compelling visuals for digital platforms. This includes creating engaging social media graphics, impactful digital advertisements, and informative infographics that capture attention and drive engagement.", brandInnercontent: "Why it matters: In today's digital landscape, strong visuals are essential for online presence. Effective digital graphics increase brand visibility, enhance social media engagement, and drive online traffic.", year: "2021", SerivesHedding: "crown digital agency site", titel: 'css design awards', image: Awardfour,
     },
     {
-      name: "Image Editing and Manipulation", year: "2020", SerivesHedding: "the journey of one project", titel: 'portfolio design', image: AwardFive,
+      name: "Image Editing and Manipulation", brandDescription: "What I do: I enhance and manipulate images to meet your specific needs. This includes photo retouching, color correction, background removal, image composition, and file preparation for both print and web.", brandInnercontent: "Why it matters: High-quality images are crucial for all design projects. Professional image editing ensures that your visuals are polished, consistent, and optimized for their intended use.", year: "2020", SerivesHedding: "the journey of one project", titel: 'portfolio design', image: AwardFive,
     },
   ]
+
 
 
 
@@ -82,24 +89,53 @@ const Home = () => {
             </div>
             <div className="col-lg-6 col-md-12 col-sm-12 col-12">
               <h2 className='fw-bold mt-3'>
-              Building powerful brands through creative design, seamlessly navigating the digital world to drive growth and visibility.</h2>
+                Building powerful brands through creative design, seamlessly navigating the digital world to drive growth and visibility.</h2>
               <div className=' mt-100'>
-                <Link className='text-decoration-none'>
-                  <ul className=' p-0'>
-                    {
-                      MainArr.map((SerivesValue, index) => (
-                        <div key={index} className='d-flex justify-content-between align-items-center custom-table-list'>
-                          <li className='text-capitalize'>
-                            {SerivesValue.name}
-                          </li>
-                          <span>
-                            <i className="fa-solid fa-arrow-right"></i>
-                          </span>
-                        </div>
-                      ))
-                    }
-                  </ul>
-                </Link>
+                <ul className="p-0">
+                  {MainArr.map((service, index) => (
+                    <li key={index} className="list-unstyled">
+                      <div
+                        className="d-flex justify-content-between align-items-center custom-table-list"
+                        onClick={() => handleToggle(index)}
+                        style={{ cursor: "pointer" }}
+                      >
+                        <span className="text-capitalize fw-bold">{service.name}</span>
+                        <i
+                          className={`fa-solid fa-arrow-right ${activeIndex === index ? "rotate" : ""}`}
+                        ></i>
+                      </div>
+
+                      {activeIndex === index && (
+                        <>
+                          {service.brandDescription && (
+                            <div className="content-box d-flex gap-3">
+                              <i className="fa-solid fa-circle li-icon-dots mt-2"></i>
+                              <p>
+                                <span style={{ fontWeight: "bold", fontSize: "17px", paddingRight: "10px" }}>
+                                  {service.brandDescription.split(" ").slice(0, 3).join(" ")}
+                                </span>
+                                {service.brandDescription.split(" ").slice(3).join(" ")}
+                              </p>
+                            </div>
+                          )}
+
+                          {service.brandInnercontent && (
+                            <div className="content-box d-flex gap-3">
+                              <i className="fa-solid fa-circle li-icon-dots mt-2"></i>
+                              <p>
+                                <span style={{ fontWeight: "bold", fontSize: "17px", paddingRight: "10px" }}>
+                                  {service.brandInnercontent.split(" ").slice(0, 3).join(" ")}
+                                </span>
+                                {service.brandInnercontent.split(" ").slice(3).join(" ")}
+                              </p>
+                            </div>
+                          )}
+                        </>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+
               </div>
             </div>
           </div>
@@ -174,7 +210,7 @@ const Home = () => {
         </div>
       </section>
 
-      <section className= 'mb-50 custom-my'>
+      <section className='mb-50 custom-my'>
         <div className="row m-0 p-0 ">
           <div className="col-lg-3 col-md-6 col-sm-6 col-12 offset-lg-3">
             <div className="blog-container">
@@ -215,7 +251,7 @@ const Home = () => {
         </div>
       </section>
 
-      <Footer/>
+      <Footer />
     </>
   )
 }
